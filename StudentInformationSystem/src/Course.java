6c246a6bd08b60dd373d2429d74e2d7573ab35cd
Zhiyuan Huang <Huangzhiyuan999@gmail.com>
2016-11-14 10:59:38 -0700

    Constructor completed

--- StudentInformationSystem/src/Course.java
+++ StudentInformationSystem/src/Course.java
@@ -1,5 +1,37 @@
 
 public class Course
 	{
-
+		private String className;
+		private int period;
+		private String grade;
+		public Course(String cn, int p, String g)
+		{
+			className = cn;
+			period = p;
+			grade = g;
+		}
+		public String getClassName()
+			{
+				return className;
+			}
+		public void setClassName(String className)
+			{
+				this.className = className;
+			}
+		public int getPeriod()
+			{
+				return period;
+			}
+		public void setPeriod(int period)
+			{
+				this.period = period;
+			}
+		public String getGrade()
+			{
+				return grade;
+			}
+		public void setGrade(String grade)
+			{
+				this.grade = grade;
+			}
 	}
--- StudentInformationSystem/src/Student.java
+++ StudentInformationSystem/src/Student.java
@@ -1,5 +1,67 @@
 
 public class Student
 	{
-
+		private String firstName;
+		private String lastName;
+		private double GPA;
+		private Course class1;
+		private Course class2;
+		private Course class3;
+		public Student(String fn, String ln, double g, Course c1, Course c2, Course c3)
+		{
+			firstName = fn;
+			lastName = ln;
+			GPA = g;
+			class1 = c1;
+			class2 = c2;
+			class3 = c3;
+		}
+		public String getFirstName()
+			{
+				return firstName;
+			}
+		public void setFirstName(String firstName)
+			{
+				this.firstName = firstName;
+			}
+		public String getLastName()
+			{
+				return lastName;
+			}
+		public void setLastName(String lastName)
+			{
+				this.lastName = lastName;
+			}
+		public double getGPA()
+			{
+				return GPA;
+			}
+		public void setGPA(double gPA)
+			{
+				GPA = gPA;
+			}
+		public Course getClass1()
+			{
+				return class1;
+			}
+		public void setClass1(Course class1)
+			{
+				this.class1 = class1;
+			}
+		public Course getClass2()
+			{
+				return class2;
+			}
+		public void setClass2(Course class2)
+			{
+				this.class2 = class2;
+			}
+		public Course getClass3()
+			{
+				return class3;
+			}
+		public void setClass3(Course class3)
+			{
+				this.class3 = class3;
+			}
 	}
